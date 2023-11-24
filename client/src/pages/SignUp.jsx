3e89b64b6a20e1700001b3fcf2b3b5fceda2a874
Orkhan1990 <orkhan.talibli@gmail.com>
2023-11-24 17:33:16 +0400

    signup page and functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,33 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
+    const [formData,setFormData]=useState({});
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        const res=await fetch("/api/v1/signUp",
+        {
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body:JSON.stringify(formData)
+        });
+
+        const data=await res.json();
+       console.log(data)
+    }
+
+    const handleChange=(e)=>{
+         setFormData({...formData,[e.target.id]:e.target.value})
+    }
   return (
     <div className="p-3 max-w-lg mx-auto">
         <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
-        <form className="flex flex-col gap-4 ">
-          <input type="text"  placeholder="username" id="username" className="border p-3 rounded-large focus:outline-none"/>
-          <input type="email" placeholder="email" id="email"  className="border p-3 rounded-large focus:outline-none"/>
-          <input type="password" placeholder="password" id="password" className="border p-3 rounded-large focus:outline-none"/>
-         <button className="bg-slate-700 text-white uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
-        <button className="bg-red-700 text-white uppercase hover:opacity-95 disabled:opacity-80">Continue with google</button>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+          <input onChange={handleChange} type="text"  placeholder="username" id="username" className="border p-3 rounded-large focus:outline-none"/>
+          <input onChange={handleChange} type="email" placeholder="email" id="email"  className="border p-3 rounded-large focus:outline-none"/>
+          <input onChange={handleChange} type="password" placeholder="password" id="password" className="border p-3 rounded-large focus:outline-none"/>
+         <button className="bg-slate-700 text-white rounded-lg p-2 uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
+        <button className="bg-red-700 text-white rounded-lg p-2 uppercase hover:opacity-95 disabled:opacity-80">Continue with google</button>
         </form>
 
         <div className="flex gap-2 mt-5">
