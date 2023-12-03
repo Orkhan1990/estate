cd9f48756c6d25451e515808e8e00bbda0ba0805
Orkhan Talibli <orkhan.talibli@gmail.com>
2023-12-03 14:37:53 +0400

    creating listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,21 +1,120 @@
-
 const Listing = () => {
   return (
-    <div>
-        <h1>Create a Listing</h1>
+    <main className="p-3 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">
+        Create a Listing
+      </h1>
 
-        <form >
-            <div>
-            <input type="text" placeholder="Name" id="name"/>
-            <input type="text" placeholder="Description" id="description"/>
-            <input type="text" placeholder="Address" id="address"/>
+      <form className="flex flex-col sm:flex-row">
+        <div className="flex flex-col gap-4 flex-1">
+          <input
+            type="text"
+            placeholder="Name"
+            id="name"
+            className="border p-3 rounded-lg"
+            maxLength="62"
+            minLength="10"
+            required
+          />
+          <textarea
+            className="border p-3 rounded-lg"
+            type="text"
+            placeholder="Description"
+            id="description"
+            required
+          />
+          <input
+            className="border p-3 rounded-lg"
+            type="text"
+            placeholder="Address"
+            id="address"
+            required
+          />
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-2">
+              <input type="checkbox" id="sell" className="w-5" />
+              <span>Sell</span>
             </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking spot</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
+          </div>
         
+          <div className="flex flex-col gap-3 ">
+            <div className="flex items-center gap-2 ">
+              <input
+                type="number"
+                id="bed"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg "
+                defaultValue={0}
+                
+              />
+              <span>Beds</span>
+            </div>
 
-        </form>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bath"
+                min={"1"}
+                max={"10"}
+                required
+                defaultValue={0}
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <span>Baths</span>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="regularPrice"
+                required
+                defaultValue={0}
+                className="p-3 border border-gray-300 rounded-lg"
+                min="1"
+                max="10"
+              />
+              <div className="flex flex-col">
+                <span>Regular Price</span>
+                <span className="text-xs">($/month)</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="discountedPrice"
+                required
+                defaultValue={0}
+                className="p-3 border border-gray-300 rounded-lg "
+                min="1"
+                max="10"
+              />
+              <div className="flex flex-col">
+                <span>Discounted Price </span>
+                <span className="text-xs">($/month)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </main>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Listing
+export default Listing;
